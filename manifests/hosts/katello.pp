896914b4d05640d56f23fec11ef01dcef46d9129
Kris Buytaert <github@krisbuytaert.be>
2011-10-29 15:23:10 +0200

    Still dependency ieeuse .. nss needs a more recent version .. Requires: nss >= 3.12.9

--- manifests/hosts/katello.pp
+++ manifests/hosts/katello.pp
@@ -3,32 +3,61 @@ node 'katello' {
 
 	yumrepo {
 
+
+		'candlepin':	
+			enabled => 1,
+			baseurl => 'http://repos.fedorapeople.org/repos/candlepin/candlepin/epel-6Server/$basearch/',	
+			gpgcheck => 0;
+
+
+		'pulp':
+
+			enabled => 1 ,
+			baseurl => 'http://repos.fedorapeople.org/repos/pulp/pulp/6Server/$basearch/',
+			gpgcheck => 0;
+
+
+
+
 		'katello':
 			enabled => 1 ,
-                        baseurl => "http://repos.fedorapeople.org/repos/katello/katello/fedora-15/i386/",
-			gpgcheck => 0,
+				baseurl => "http://repos.fedorapeople.org/repos/katello/katello/6Server/x86_64/",
+				gpgcheck => 0;
+
+		'epel':
+			baseurl => $operatingsystemrelease ? {
+				'6.0' => "http://mirror.eurid.eu/epel/6/$hardwaremodel/",
+					'*' => "http://mirror.eurid.eu/epel/5/$hardwaremodel/",
+			},
+				descr => $operatingsystemrelease ? {
+					'6.0' => 'Extra Packages for Enterprise Linux 6.x
+						',
+					'*' => 'Extra Packages for Enterprise Linux 5.x',
+				},
+				gpgcheck => 0,
+				enabled => 1;
 
 
 
-        }
+	}
 
 
 
-	
 
 
-  	service { "iptables":
-                ensure => "stopped",
-                enable => "false";
-                }
-	
+
+	service { "iptables":
+		ensure => "stopped",
+		       enable => "false";
+	}
+
 	package {
-		
+
 		"katello-all":
 			ensure => "present";
 	}
 
-	
+
 
 }
 
