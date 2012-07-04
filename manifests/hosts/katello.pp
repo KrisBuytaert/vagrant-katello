9b82289125bafada0021a89b09a9908c33c54b38
Kris Buytaert <sdog@inuits.be>
2012-07-04 15:25:22 +0200

    Adding correct repos

--- manifests/hosts/katello.pp
+++ manifests/hosts/katello.pp
@@ -1,68 +1,63 @@
 node 'katello' {
 
+  yumrepo {
+    'epel':
+      baseurl => $operatingsystemrelease ? {
+        '6.0' => "http://mirror.eurid.eu/epel/6/${hardwaremodel}/",
+        '*'   => "http://mirror.eurid.eu/epel/5/${hardwaremodel}/",
+      },
+      descr   => $operatingsystemrelease ? {
+        '6.0' => 'Extra Packages for Enterprise Linux 6.x ',
+        '*'   => 'Extra Packages for Enterprise Linux 5.x',
+      },
+      gpgcheck => 0,
+      enabled  => 1;
 
-	yumrepo {
-
-
-		'candlepin':	
-			enabled => 0,
-			baseurl => 'http://repos.fedorapeople.org/repos/candlepin/candlepin/epel-6Server/$basearch/',	
-			gpgcheck => 0;
-
-
-		'pulp':
-
-			enabled => 1 ,
-			baseurl => 'http://repos.fedorapeople.org/repos/pulp/pulp/6Server/$basearch/',
-			gpgcheck => 0;
-
+  }
 
 
 
-		'katello':
-			enabled => 1 ,
-				baseurl => "http://repos.fedorapeople.org/repos/katello/katello/6Server/x86_64/",
-				gpgcheck => 0;
-
-		'epel':
-			baseurl => $operatingsystemrelease ? {
-				'6.0' => "http://mirror.eurid.eu/epel/6/$hardwaremodel/",
-					'*' => "http://mirror.eurid.eu/epel/5/$hardwaremodel/",
-			},
-				descr => $operatingsystemrelease ? {
-					'6.0' => 'Extra Packages for Enterprise Linux 6.x
-						',
-					'*' => 'Extra Packages for Enterprise Linux 5.x',
-				},
-				gpgcheck => 0,
-				enabled => 1;
-
-
-	}
-
-
-
-
-
-
-	service { "iptables":
-		      ensure => "stopped",
-		      enable => "false";
-
-# When you run into AutoReconnect: could not find master/primary
-# It means mongodb ain't running 
-
-	}
-
-	package {
-		"centos-release-cr":
-			ensure => "present";
-#		"katello":
-#			ensure => "present";
-	}
+  yumrepo { 'pulp-v1-stable':
+    baseurl   => 'http://repos.fedorapeople.org/repos/pulp/pulp/v1/stable/$releasever/$basearch/',
+    descr     => 'Pulp v1',
+    enabled   => '1',
+    gpgcheck  => '0',
+  }
+  yumrepo { 'pulp-v1-testing':
+    baseurl   => 'http://repos.fedorapeople.org/repos/pulp/pulp/v1/testing/$releasever/$basearch/',
+    descr     => 'Pulp v1 Testing',
+    enabled   => '0',
+    gpgcheck  => '0',
+  }
+  yumrepo { 'pulp-dev-cr':
+    baseurl   => 'http://repos.fedorapeople.org/repos/pulp/pulp/dev/stable/$releasever/$basearch/',
+    descr     => 'Pulp Community Releases',
+    enabled   => '0',
+    gpgcheck  => '0',
+  }
+  yumrepo { 'pulp-dev-testing':
+    baseurl   => 'http://repos.fedorapeople.org/repos/pulp/pulp/dev/testing/$releasever/$basearch/',
+    descr     => 'Pulp Testing Builds',
+    enabled   => '0',
+    gpgcheck  => '0',
+  }
 
 
-	include pulp 
+
+  service { 'iptables':
+    ensure => 'stopped',
+    enable => 'false';
+
+
+  }
+
+  package {
+    'centos-release-cr':
+      ensure => 'present';
+    }
+
+
+    include pulp
 }
 
 
